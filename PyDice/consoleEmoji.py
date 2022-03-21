18ca14ea9a2b8f4c48e049da444fe254772017bb
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-03-22 00:21:48 +0530

    Update consoleEmoji.py

--- PyDice/consoleEmoji.py
+++ PyDice/consoleEmoji.py
@@ -3,4 +3,4 @@ import random as r
 while True:
     if 'x' in input("Roll Dice ( x for eXit) ? : "):
         exit(0)
-    print((""+r.choice(["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"])).center(28))
+    print(r.choice(["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]).center(28))
