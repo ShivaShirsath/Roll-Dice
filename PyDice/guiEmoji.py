9e14d86dbd6fca893bfa50e5b2dfa9e78c2836e7
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-03-22 00:18:02 +0530

    Create guiEmoji.py

---
+++ PyDice/guiEmoji.py
@@ -0,0 +1,16 @@
+import tkinter as tk, time as t, random as r
+
+root = tk.Tk()
+
+root.title('Roll Dice')
+root.geometry('200x100')
+root.eval('tk::PlaceWindow . center')
+
+def getDice():
+    t.sleep(1.5)
+    b["text"]=("" + r.choice(["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]))
+    
+b = tk.Button(root, text=root.title(), command=getDice)
+b.pack(expand=True)
+
+root.mainloop()
